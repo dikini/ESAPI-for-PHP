b5f43530ad6f114ca9c6f8f22936de2f1e6d9a5f
Brian Fenton <bfenton@ku.edu>
2011-06-19 22:23:49 -0500

    spacing, and switched StringBuffer to string (java)

--- src/StringUtilities.php
+++ src/StringUtilities.php
@@ -4,7 +4,7 @@
  *
  * This file is part of the Open Web Application Security Project (OWASP)
  * Enterprise Security API (ESAPI) project.
- * 
+ *
  * PHP version 5.2
  *
  * LICENSE: This source file is subject to the New BSD license.  You should read
@@ -24,7 +24,7 @@
 /**
  * Use this ESAPI security control to assist with manipulating strings
  * in other ESAPI security controls.
- * 
+ *
  * The idea behind this interface is to define a set of helper
  * functions related to manipulating strings.
  *
@@ -41,14 +41,14 @@ class StringUtilities
 {
 
     /**
-     * Removes all unprintable characters from a string 
+     * Removes all unprintable characters from a string
      * and replaces with a space for use in an HTTP header
-     * 
+     *
      * @param string $input a string that may have unprintable characters
-     * 
+     *
      * @return string the stripped header
      */
-    public static function stripControls( $input ) 
+    public static function stripControls( $input )
     {
         if (empty($input)) {
             return '';
@@ -71,13 +71,13 @@ class StringUtilities
 
     /**
      * Union two character arrays.
-     * 
+     *
      * @param string $c1 the first character array
      * @param string $c2 the second character array
-     * 
+     *
      * @return array the union of the two character arrays
      */
-    public static function union($c1, $c2) 
+    public static function union($c1, $c2)
     {
         if (empty($c1) && empty($c2)) {
             return null;
@@ -88,14 +88,14 @@ class StringUtilities
 
 
     /**
-     * Returns true if the character is contained in the provided StringBuffer.
+     * Returns true if the character is contained in the provided string.
      * 
      * @param string $haystack the string to search
      * @param string $c        the character to search for in the string
-     * 
+     *
      * @return bool TRUE, if the character is found, false otherwise
      */
-    public static function contains($haystack, $c) 
+    public static function contains($haystack, $c)
     {
         if ( empty($haystack) || empty($c) ) {
             return false;
