a49ca6b2b1b41837c0df19bbb7761f6760adcacd
Brian Fenton <bfenton@ku.edu>
2011-06-19 23:00:39 -0500

    spacing

--- src/reference/DefaultExecutor.php
+++ src/reference/DefaultExecutor.php
@@ -4,7 +4,7 @@
  *
  * This file is part of the Open Web Application Security Project (OWASP)
  * Enterprise Security API (ESAPI) project.
- * 
+ *
  * PHP version 5.2
  *
  * LICENSE: This source file is subject to the New BSD license.  You should read
@@ -13,7 +13,7 @@
  *
  * @category  OWASP
  * @package   ESAPI_Reference
- * @author    Mike Boberski <boberski_michael@bah.com> 
+ * @author    Mike Boberski <boberski_michael@bah.com>
  * @author    Linden Darling <linden.darling@jds.net.au>
  * @copyright 2009-2010 The OWASP Foundation
  * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
@@ -28,7 +28,7 @@ require_once  dirname(__FILE__).'/../Executor.php';
  *
  * @category  OWASP
  * @package   ESAPI_Reference
- * @author    Mike Boberski <boberski_michael@bah.com> 
+ * @author    Mike Boberski <boberski_michael@bah.com>
  * @author    Linden Darling <linden.darling@jds.net.au>
  * @copyright 2009-2010 The OWASP Foundation
  * @license   http://www.opensource.org/licenses/bsd-license.php New BSD license
@@ -37,7 +37,7 @@ require_once  dirname(__FILE__).'/../Executor.php';
  */
 class DefaultExecutor implements Executor
 {
-        
+
     // Logger
     private $_auditor = null;
     private $_ApplicationName = null;
@@ -48,10 +48,10 @@ class DefaultExecutor implements Executor
     
     //SecurityConfiguration
     private $_config = null;
-    
+
     /**
      * Executor constructor.
-     * 
+     *
      * @return does not return a value.
      */
     function __construct()
@@ -71,41 +71,41 @@ class DefaultExecutor implements Executor
             $executable, $params, $workdir, $logParams
         );
     }
-     
+
      /**
      * @inheritdoc
      */
-    function executeSystemCommandLonghand($executable, $params, $workdir, 
+    function executeSystemCommandLonghand($executable, $params, $workdir,
         $logParams
     ) {
         try {
-            
+
             // executable must exist
             $resolved = $executable;
-            
+
             if (substr(PHP_OS, 0, 3) == 'WIN') {
                 $exploded = explode("%", $executable);
                 $systemroot = getenv($exploded[1]);
                 $resolved = $systemroot . $exploded[2];
             }
-            
+
             if (!file_exists($resolved)) {
                 throw new ExecutorException(
                     "Execution failure, No such ".
                     "executable: $executable"
                 );
             }
-            
+
             // executable must use canonical path
             if (strcmp($resolved, realpath($resolved)) != 0) {
                 throw new ExecutorException(
                     "Execution failure, Attempt ".
                     "to invoke an executable using a non-absolute path: [".realpath($resolved)."] != [$executable]"
                 );
-            }            
-                             
-            // exact, absolute, canonical path to executable must be listed 
-            //in ESAPI configuration 
+            }
+
+            // exact, absolute, canonical path to executable must be listed
+            //in ESAPI configuration
             $approved = $this->_config->getAllowedExecutables();
             if (!in_array($executable, $approved)) {
                 throw new ExecutorException(
@@ -113,13 +113,13 @@ class DefaultExecutor implements Executor
                     "is not listed as an approved executable in ESAPI ".
                     "configuration: ".$executable . " not listed in " . $approved
                 );
-            }            
+            }
 
             // escape any special characters in the parameters
             for ($i = 0; $i < count($params); $i++) {
-                $params[$i] = escapeshellcmd($params[$i]);  
+                $params[$i] = escapeshellcmd($params[$i]);
             }
-           
+
             // working directory must exist
             $resolved_workdir = $workdir;
             if (substr(PHP_OS, 0, 3) == 'WIN') {
@@ -136,24 +136,24 @@ class DefaultExecutor implements Executor
                     " working directory for running executable: $workdir"
                 );
             }
- 
+
             // run the command
             $paramstr = "";
             foreach ($params as $param) {
                 //note: will yield a paramstr with a leading whitespace
-                $paramstr .= " ".$param;    
+                $paramstr .= " ".$param;
             }
-            //note: no whitespace between $executable and $paramstr since 
+            //note: no whitespace between $executable and $paramstr since
             //$paramstr already has a leading whitespace
-            $output = shell_exec($executable . $paramstr);    
+            $output = shell_exec($executable . $paramstr);
             return $output;
         }
         catch ( ExecutorException $e ) {
             $this->_auditor->warning(Auditor::SECURITY, true, $e->getMessage());
             throw new ExecutorException($e->getMessage());
         }
-    
-    }    
-    
+
+    }
+
 }
 ?>
